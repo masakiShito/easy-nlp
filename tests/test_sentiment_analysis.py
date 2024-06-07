391eb9652657e0b11e3cfcd3a1fd2b8d059edb51
masakiShito <happy.engineer.life@gmail.com>
2024-06-07 19:59:51 +0900

    修正

--- tests/test_sentiment_analysis.py
+++ tests/test_sentiment_analysis.py
@@ -16,3 +16,21 @@ class TestSentimentAnalyzer(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+import unittest
+from easynlp.sentiment_analysis import SentimentAnalyzer
+
+class TestSentimentAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        self.analyzer = SentimentAnalyzer()
+
+    def test_analyze_positive(self):
+        result = self.analyzer.analyze("I love this library!")
+        self.assertEqual(result[0]['label'], 'POSITIVE')
+
+    def test_analyze_negative(self):
+        result = self.analyzer.analyze("I hate this library!")
+        self.assertEqual(result[0]['label'], 'NEGATIVE')
+
+if __name__ == '__main__':
+    unittest.main()
